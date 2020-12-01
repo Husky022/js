d49c2e5aacdf69ff23f9aa2b6db3dd4f88e16c9c
Ilya <balashovia@bk.ru>
2020-12-01 11:27:58 +0300

    Lesson2

---
+++ Lesson2/task2.3.js
@@ -0,0 +1,15 @@
+var a = -15;
+var b = 10;
+
+if (a >= 0 && b >= 0) {
+    var result = a - b
+    console.log(result)    
+}
+else if (a < 0 && b < 0) {
+    var result = a * b
+    console.log(result)
+}
+else {
+    var result = a + b
+    console.log(result)
+}
